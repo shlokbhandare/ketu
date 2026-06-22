c5019dfb883ea103fab87ba9ad729fbcfdfb587f
EquinoxialSicko <shlokbzard@gmail.com>
2026-06-22 18:06:03 +0530

    feat: backend pool struct with round-robin

--- src/backend.rs
+++ src/backend.rs
@@ -1,9 +1,22 @@
+use std::sync::{Arc, Mutex};
+
 pub struct BackendPool {
     pub backends: Vec<String>,
+    current_index: Arc<Mutex<usize>>,
 }
 
 impl BackendPool {
     pub fn new(backends: Vec<String>) -> Self {
-        Self { backends }
+        Self {
+            backends,
+            current_index: Arc::new(Mutex::new(0)),
+        }
+    }
+
+    pub fn next(&self) -> String {
+        let mut current_index = self.current_index.lock().unwrap();
+        let backend = self.backends[*current_index].clone();
+        *current_index = (*current_index + 1) % self.backends.len();
+        backend
     }
 }
